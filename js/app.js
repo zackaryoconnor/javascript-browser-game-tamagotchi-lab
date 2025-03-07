dba8a4de9b3a9f0254f9a34af400edcd0d9a26d8
Zack OConnor <zackary25@gmail.com>
2025-03-06 21:11:46 -0500

    Added random state and a random amount to increment the state by.

--- js/app.js
+++ js/app.js
@@ -6,26 +6,66 @@ const state = {
 
 
 let timeElapsed = 0;
+let randomNumber;
+let randomState = ``;
 
-
-const statsWrapper = document.querySelector(`#stats-wrapper`);
+const buttonWrapperElement = document.querySelector(`.button-wrapper`);
+let boredomLabel = document.querySelector(`#boredom-stat`);
+let hungerLabel = document.querySelector(`#hunger-stat`);
+let sleepinessLabel = document.querySelector(`#sleepiness-stat`);
 
 
 const interval = setInterval(() => {
-    timeElapsed++;
-    console.log(timeElapsed)
+    timeElapsed += 1
+    console.log(timeElapsed, `seconds passed`);
     if (timeElapsed % 5 === 0) {
-        updateLabel();
+        getRandomNumber();
+        getRandomState();
+        updateLabel(randomState);
     }
 
     if (timeElapsed >= 30) {
-        clearInterval(interval)
+        clearInterval(interval);
     }
-}, 1000)
+}, 1000);
 
 
-function updateLabel() {
-    let boredomLabel = document.querySelector(`#boredom-stat`);
-    state.boredom += 1;
-    boredomLabel.textContent = state.boredom;
+const updateLabel = label => {
+    if (label === `boredom`) {
+        state.boredom += randomNumber;
+        boredomLabel.textContent = state.boredom;
+    } else if (label === `hunger`) {
+        state.hunger += randomNumber;
+        hungerLabel.textContent = state.hunger;
+    } else if (label === `sleepiness`) {
+        state.sleepiness += randomNumber;
+        sleepinessLabel.textContent = state.sleepiness;
+    }
 }
+
+
+const getRandomNumber = () => {
+    randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(`random number`, randomNumber)
+}
+
+
+const getRandomState = () => {
+    const keys = Object.keys(state);
+    randomState = keys[Math.floor(Math.random() * keys.length)];
+    console.log(`random state:`, randomState)
+};
+
+
+buttonWrapperElement.addEventListener('click', event => {
+    if (event.target.textContent === `play`) {
+        boredomLabel.textContent = `0`;
+        state.boredom = 0;
+    } else if (event.target.textContent === `feed`) {
+        state.hunger = 0;
+        hungerLabel.textContent = `0`;
+    } else if (event.target.textContent === `sleep`) {
+        state.sleepiness = 0;
+        sleepinessLabel.textContent = `0`;
+    }    
+});
